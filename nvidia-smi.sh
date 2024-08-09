aae249bf19b1a307471b1fd06caabf1b767b1c1b
LucasBeerens <61274423+LucasBeerens@users.noreply.github.com>
2024-08-09 16:06:19 +0100

    Update nvidia-smi.sh

--- nvidia-smi.sh
+++ nvidia-smi.sh
@@ -3,7 +3,7 @@
 # Namespace
 namespace='eidf151ns'
 # Get run job name
-job_name='lb-deceptive-diffusion-job'
+job_name='example-job'
 
 # Get the pod name of the pod attached to the run job
 pod_name=$(kubectl -n $namespace get pod -l job-name=$job_name -o jsonpath="{.items[0].metadata.name}")
